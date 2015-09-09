62e6818f9d7440404bb232cd58cdee233ae01ba0
Richard Bota <richard.bota@khk.ee>
2015-09-09 12:20:21 +0300

    Esilehele lisada loend faktidest
    [Delivers #102983342]

---
+++ index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<br lang="en">
+<head>
+
+    <link type="text/css" rel="stylesheet" href="stylesheet.css"/>
+    <meta charset="UTF-8">
+    <title>Hardi tutvustus</title>
+
+</head>
+<body>
+<h1>Hardi Kaiv</h1>
+
+<h2>Lühitutvustus</h2>
+<ul>
+    <li>19 aastane</li>
+    <li>Sündinud Võrus</li>
+    <li>Lõpetas Võru Kreutzwaldi Gümnaasiumi</li>
+</ul>
+
+<h2>Tehtud tööd</h2>
+
+<a href="https://www.codecademy.com/byteBlaster79419">Codecademy</a>
+
+</body>
+</html>
